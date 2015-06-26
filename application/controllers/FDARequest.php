78e65618f9bd84d43931884fc6e1068896712805
brown078 <damon@technicate.com>
2015-06-25 23:45:38 -0700

    Adding brand name search to initial API

--- application/controllers/FDARequest.php
+++ application/controllers/FDARequest.php
@@ -28,7 +28,17 @@ class FDARequest extends CI_Controller {
         {
             //using fopen for no curl requirements, all FDA requests are get requests
             $data = file_get_contents('https://api.fda.gov/drug/label.json?api_key=TwOhEwWWk1eGlkFl8n1HHCn93bmJity6AZbQGHpj&search=openfda.brand_name:"'.$search.'"');
-            echo $data;
+            //seperate the JSON request into array for results review
+            $data  = json_decode($data);
+            if($data->error){
+                //search generic drug information
+                $data = file_get_contents('https://api.fda.gov/drug/label.json?api_key=TwOhEwWWk1eGlkFl8n1HHCn93bmJity6AZbQGHpj&search=openfda.generic_name:"'.$search.'"');
+                //seperate the JSON request into array for results review
+                $data  = json_decode($data);
+            }
+            echo"<pre>";
+            print_r($data);
+            echo"</pre>";
             
         }
         
