09a77e1598b328ecdd11059a3e578d06b2b6a2bb
brown078 <damon@technicate.com>
2015-06-26 17:07:20 -0700

    modified the JSON return to include content type

--- application/controllers/FDARequest.php
+++ application/controllers/FDARequest.php
@@ -3,33 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class FDARequest extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	/*
+         * Index a standard message output unless there is a request
+         * 
+         * 
+         * 
+         */
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
-        //TwOhEwWWk1eGlkFl8n1HHCn93bmJity6AZbQGHpj
-        //C:\Users\damon\Documents\NetBeansProjects\RFQ993471
+       
         public function drugsearch($search)
         {
 			$this->load->model('fda_model');
+                        
+                        $this->output->set_content_type('application/json')->set_output($this->fda_model->search($search));
 
-
-			echo $this->fda_model->search($search);
         }
         
         
